2eaa5dfb47bf6e7e524ab9b77ed9b8b55db775fb
DAMO <27757251@qq.com>
2016-06-21 23:53:50 +0800

    Modified some command

--- apk.sh
+++ apk.sh
@@ -7,7 +7,7 @@ if [ $# == 1 ]; then
 		return
 	fi
 	cd $apkPath
-	
+
 	ant release
 	jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -signedjar ./bin/APK.apk ./bin/APK-release-unsigned.apk androiddebugkey
 else
