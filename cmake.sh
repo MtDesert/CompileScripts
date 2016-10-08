0db6f9d45da7fb4b3871e0cd64cb61ef7fca2c79
Damo <27757251@qq.com>
2016-10-08 19:54:26 +0800

    Adjust code

--- cmake.sh
+++ cmake.sh
@@ -4,17 +4,17 @@ if [ $# == 2 ]; then
 	platformName=$2
 	
 	#execute shell
-	if [ ! -d $projectPath ]; then
-		return
+	if [ -d $projectPath ]; then
+		cd $projectPath
+		if [ ! -d build-${platformName} ]; then
+			mkdir build-${platformName}
+		fi
+		cd build-${platformName}
+		cmake -DCMAKE_SYSTEM_NAME=${platformName} ..
+		sudo make
+	else
+		echo "No such directory $projectPath"
 	fi
-	cd $projectPath
-	
-	if [ ! -d build-${platformName} ]; then
-		mkdir build-${platformName}
-	fi
-	cd build-${platformName}
-	cmake -DCMAKE_SYSTEM_NAME=${platformName} ..
-	sudo make
 else
 	echo "Syntax: $0 ProjectDirectory(which contain CMakeLists.txt) PlatformName(Linux,Android,etc.)"
 fi
