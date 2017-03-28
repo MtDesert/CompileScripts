e6c8c369f0d386a1149468aa7e59519635354c36
githubUsername <github@eletronic.mail>
2017-03-28 18:00:02 +0800

    break if compile error

--- cmakeAllSys.sh
+++ cmakeAllSys.sh
@@ -4,6 +4,9 @@ if [ $# == 1 ]; then
 	for sys in Linux Android #compile one project to all the target system. See ./CMakeLists.txt
 	do
 		$scriptDir/cmake.sh $1 $sys
+		if [ $? != 0 ]; then #break if error appear while compiling
+			break
+		fi
 	done
 else
 	echo "Syntax: $0 ProjectDirectory(which contain CMakeLists.txt)"
