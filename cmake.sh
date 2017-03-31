4b46a1f41bcf2c8ea0350fecf4de63bb7d7c7932
Damo <27757251@qq.com>
2017-04-01 02:09:19 +0800

    Experiment successfully

--- cmake.sh
+++ cmake.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
+mkdirp()
+{
+	mkdir -p $1
+	if [ $? != 0 ];then
+		exit
+	fi
+}
+
+workingDirectory=`pwd`
 if [ $# == 2 ]; then
-	#set parameter
 	projectPath=$1
-	targetSys=$2
-	
-	#execute shell
-	if [ -d $projectPath ]; then
-		cd $projectPath
-		if [ ! -d build-${targetSys} ]; then
-			mkdir build-${targetSys}
-		fi
-		cd build-${targetSys}
-		cmake -DTARGET_SYSTEM_NAME=${targetSys} ..
+	platform=$2
+	#first, projectPath must exist
+	if [ ! -d $projectPath ]; then
+		echo "No project directory $projectPath"
+		exit
+	fi
+	#second, maybe we need a directory to put the compiled object of our object(for make our project directory clean)
+	objectPath=obj #(change the name as you like)
+	mkdirp $objectPath
+	#third, make a directory name $projectPath-$platform
+	cd $objectPath
+	mkdirp $projectPath-$platform
+	cd $workingDirectory
+	#fourth, check platform and do theirselves' methods
+	if [ $platform == Linux -o $platform == Android ]; then #we can use cmake, and then make
+		cd $workingDirectory/$objectPath/$projectPath-$platform
+		cmake -DTARGET_SYSTEM_NAME=${platform} ../../$projectPath
 		sudo make
+	elif [ $platform == Mac ]; then #not sure
+		echo "How to build for Mac????"
+	elif [ $platform == Windows ]; then #not sure
+		echo "How to build for Windows????"
 	else
-		echo "No such directory $projectPath"
+		echo "Unknown platform: $platform"
 	fi
 else
-	echo "Syntax: $0 ProjectDirectory(which contain CMakeLists.txt) TargetSystemName(Linux,Android,etc.)"
+	echo "Syntax: make.sh projectDir targetPlatform"
 fi
