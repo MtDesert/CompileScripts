a48389d53affbc7eb96b7dc76f72252ce5740e79
githubUsername <github@eletronic.mail>
2017-04-01 15:55:49 +0800

    Rebuildgit checkoutgit checkout

--- cmake.sh
+++ cmake.sh
@@ -7,6 +7,7 @@ mkdirp()
 	fi
 }
 
+#main()
 workingDirectory=`pwd`
 if [ $# == 2 ]; then
 	projectPath=$1
@@ -17,16 +18,22 @@ if [ $# == 2 ]; then
 		exit
 	fi
 	#second, maybe we need a directory to put the compiled object of our object(for make our project directory clean)
-	objectPath=obj #(change the name as you like)
+	objectPath=objs #(change the name as you like)
 	mkdirp $objectPath
-	#third, make a directory name $projectPath-$platform
+	#also, maybe we need some different directories to put different platform's libraries file and executable file
+	libPath=lib-$platform
+	mkdirp $libPath
+	exePath=exe-$platform
+	mkdirp $exePath
+
+	#third, make a directory name $projectPath-$platform in $objectPath
 	cd $objectPath
 	mkdirp $projectPath-$platform
 	cd $workingDirectory
-	#fourth, check platform and do theirselves' methods
+	#fourth, check platform and do methods of theirselves
 	if [ $platform == Linux -o $platform == Android ]; then #we can use cmake, and then make
 		cd $workingDirectory/$objectPath/$projectPath-$platform
-		cmake -DTARGET_SYSTEM_NAME=${platform} ../../$projectPath
+		cmake -DTARGET_SYSTEM_NAME=${platform} -DLIBRARY_OUTPUT_PATH=../../$libPath -DEXECUTABLE_OUTPUT_PATH=../../$exePath ../../$projectPath
 		sudo make
 	elif [ $platform == Mac ]; then #not sure
 		echo "How to build for Mac????"
---
+++ cmakeAll.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#main()
+scriptDir=`dirname $0`
+platforms="Linux Android Windows Mac"
+platform=false
+project=false
+for para in $@
+do
+	#select platform name
+	isPlatform=false
+	for pf in $platforms
+	do
+		if [ $para = $pf ]; then # $para is platform name
+			platform=$para
+			#echo "platform = $para"
+			project=false
+			isPlatform=true
+			project=false
+			break
+		fi
+	done
+	#set project name
+	if [ $isPlatform = false ]; then # $para is project name
+		project=$para
+		#echo "project = $para"
+	fi
+	#compile project-platform
+	if [ $platform != false -a $project != false ]; then
+		#echo "$project $platform"
+		$scriptDir/cmake.sh $project $platform
+	fi
+done
