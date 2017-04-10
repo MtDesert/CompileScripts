d2dfb621f5b5c5a31acf9b1db4567e7e7f5cf58b
Damo <27757251@qq.com>
2017-04-11 01:49:30 +0800

    Add Chinese comment

--- cmake.sh
+++ cmake.sh
@@ -17,29 +17,34 @@ if [ $# == 2 ]; then
 	projectPath=$1
 	platform=$2
 	#first, projectPath must exist
+	#首先,工程路径必须存在
 	if [ ! -d $projectPath ]; then
 		echo "No project directory $projectPath"
 		exit
 	fi
-	#second, maybe we need a directory to put the compiled object of our object(for make our project directory clean)
-	objectPath=objs #(change the name as you like)
+	#second, maybe we need a directory to put the compiled object of our source files(for make our project directory clean)
+	#其次,我们可能需要一个目录去装我们编译出来的中间文件(为了使我们的源码目录看起来很干净)
+	objectPath=objs #(change the name as you like,换成你想起的名字吧)
 	mkdirp $objectPath
 	#also, maybe we need some different directories to put different platform's libraries file and executable file
-	libPath=lib-$platform
+	#同样,我们可能也需要不同的目录来放置不同平台下编译出来的库文件和执行文件
+	libPath=lib-$platform #(change the name as you like,换成你想起的名字吧)
 	mkdirp $libPath
-	exePath=exe-$platform
+	exePath=exe-$platform #(change the name as you like,换成你想起的名字吧)
 	mkdirp $exePath
 
 	#third, make a directory name $projectPath-$platform in $objectPath
+	#接下来,在中间文件的目录中建立一个文件夹,以"工程路径-平台"的方式命名
 	cd $objectPath
 	mkdirp $projectPath-$platform
 	cd $workingDirectory
 	#fourth, check platform and do methods of theirselves
+	#接下来,检查一下目标平台,并执行特定的编译命令
 	if [ $platform == Linux -o $platform == Android ]; then #we can use cmake, and then make
 		cd $workingDirectory/$objectPath/$projectPath-$platform
 		cmake -DTARGET_SYSTEM_NAME=${platform} -DLIBRARY_OUTPUT_PATH=../../$libPath -DEXECUTABLE_OUTPUT_PATH=../../$exePath ../../$projectPath
 		exitWhileError
-		sudo make
+		sudo make #You also can remove 'sudo' if unneccessary,如非必要,可以移除sudo
 	elif [ $platform == Mac ]; then #not sure
 		echo "How to build for Mac????"
 	elif [ $platform == Windows ]; then #not sure
--- cmakeAll.sh
+++ cmakeAll.sh
@@ -7,7 +7,7 @@ exitWhileError()
 }
 #main()
 scriptDir=`dirname $0`
-platforms="Linux Android Windows Mac"
+platforms="Linux Android Windows Mac" #这是目前考虑的系统列表,不完善,需要补充
 platform=false
 project=false
 for para in $@
@@ -16,7 +16,7 @@ do
 	isPlatform=false
 	for pf in $platforms
 	do
-		if [ $para = $pf ]; then # $para is platform name
+		if [ $para = $pf ]; then # $para is platform name 要么参数是平台名,要么是项目名
 			platform=$para
 			#echo "platform = $para"
 			project=false
@@ -26,11 +26,11 @@ do
 		fi
 	done
 	#set project name
-	if [ $isPlatform = false ]; then # $para is project name
+	if [ $isPlatform = false ]; then # $para is project name 要么参数是平台名,要么是项目名
 		project=$para
 		#echo "project = $para"
 	fi
-	#compile project-platform
+	#compile project-platform 只要确定了项目名和目标平台名,就可以进行编译
 	if [ $platform != false -a $project != false ]; then
 		#echo "$project $platform"
 		$scriptDir/cmake.sh $project $platform
