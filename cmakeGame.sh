28d7a84e66e425ab1ab388658b650cd69b03575c
Damo <27757251@qq.com>
2018-12-04 02:19:46 +0800

    调整了游戏的编译过程

--- cmakeGame.sh
+++ cmakeGame.sh
@@ -1,24 +1,24 @@
 source `dirname $0`/shell.sh
 
 if [ $# == 1 ]; then
-	#编译游戏模块
+	#设置游戏名相关的变量
 	gameName=$1 #游戏名
 	objDir=objs/lib$gameName #中间文件的目录名
 	outputDir=$workingDirectory/$gameName #目标文件输出目录
-	mkdirp $objDir #创建目录
+	cmakeParameter="-DTARGET_SYSTEM_NAME=Linux -DLIBRARY_OUTPUT_PATH=$outputDir -DEXECUTABLE_OUTPUT_PATH=$outputDir -DGAME_NAME=$gameName" #cmake命令参数
+	#创建目录
+	mkdirp $objDir
 	exitWhenError
 	cd $objDir #切换目录以便执行cmake
-	cmake -DTARGET_SYSTEM_NAME=Linux -DLIBRARY_OUTPUT_PATH=$outputDir -DEXECUTABLE_OUTPUT_PATH=$outputDir $workingDirectory/lib$gameName
+	cmake $cmakeParameter -DGAME_LIB=1 $workingDirectory/lib$gameName
 	exitWhenError
-	make
+	make #开始编译
 	exitWhenError
-	#编译C++11基础库
 	#编译引擎模块
 	mkdirp ../libGamesEngines
 	exitWhenError
 	cd ../libGamesEngines
-	exitWhenError
-	cmake -DTARGET_SYSTEM_NAME=Linux -DLIBRARY_OUTPUT_PATH=$outputDir -DEXECUTABLE_OUTPUT_PATH=$outputDir -DGAME_NAME=$gameName $workingDirectory/libGamesEngines
+	cmake $cmakeParameter $workingDirectory/libGamesEngines
 	exitWhenError
 	make
 else
